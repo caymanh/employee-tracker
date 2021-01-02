df64582c7978910104b659c0a86485a36d040ec0
Cayman Heng <hengcayman@gmail.com>
2021-01-02 11:03:50 -0500

    pre-seeded database with dummy employee info

--- seed.sql
+++ seed.sql
@@ -26,5 +26,16 @@ CREATE TABLE employee (
     role_id INT not null,
     manager_id INT,
     PRIMARY KEY (id),
-    FOREIGN KEY (role_id) REFERENCES role (id),
+    FOREIGN KEY (role_id) REFERENCES role (id)
 );
+
+INSERT INTO department (name)
+VALUES ('Sales'), ('Engineering'), ('Finance'), ('Legal');
+
+INSERT INTO role (title, salary, department_id)
+VALUES ('Sales Lead',100000,1),('Salesperson',80000,1), ('Lead Engineer',150000,2),('Software Engineer',120000,2), ('Accountant',125000,3),('Legal Team Lead',250000,4),('Lawyer',190000,4);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('John','Doe',1,null),('Mike','Chan',2,null),('Ashley','Rodriguez',3,null),
+('Kevin','Tupik',4,null),('Malia','Brown',1,null),('Sarah','Lourd',2,null),
+('Tom','Allen',3,null),('Christian','Eckenrode',3,null);

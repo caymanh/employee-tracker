6a074f9707119f514358a0d49dc890027b88f6fe
Cayman Heng <hengcayman@gmail.com>
2021-01-02 09:15:02 -0500

    updated tables to indicate fields that are not null

--- seed.sql
+++ seed.sql
@@ -1,26 +1,30 @@
 DROP DATABASE IF EXISTS hr_DB;
+
 CREATE database hr_DB;
+
 USE hr_DB;
+
 create table department (
-    id int AUTO_INCREMENT,
-    name VARCHAR (30),
+    id int AUTO_INCREMENT not null,
+    name VARCHAR (30) not null,
     primary key (id)
 );
+
 create TABLE role (
-    id INT AUTO_INCREMENT,
-    title VARCHAR (30),
-    salary decimal,
-    department_id INT,
+    id INT AUTO_INCREMENT not null,
+    title VARCHAR (30) not null,
+    salary decimal not null,
+    department_id INT not null,
     PRIMARY KEY (id),
     foreign key (department_id) REFERENCES department (id)
 );
+
 CREATE TABLE employee (
-    id int AUTO_INCREMENT,
-    first_name VARCHAR (30),
-    last_name VARCHAR (30),
-    role_id INT,
+    id int AUTO_INCREMENT not null,
+    first_name VARCHAR (30) not null,
+    last_name VARCHAR (30) not null,
+    role_id INT not null,
     manager_id INT,
     PRIMARY KEY (id),
     FOREIGN KEY (role_id) REFERENCES role (id),
-    FOREIGN KEY (manager_id) REFERENCES employee (id)
 );
